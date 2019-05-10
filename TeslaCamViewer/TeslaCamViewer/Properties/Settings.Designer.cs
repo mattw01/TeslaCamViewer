5ef6792130beb774395b5b1174e0b6fe1011f905
Jared Bienz <jbienz@hotmail.com>
2019-05-09 21:08:54 -0500

    Changed the default option to find the drive on Startup to False. Added a Loading progress indicator and a cancel button that can be used to abort the operation.

--- TeslaCamViewer/TeslaCamViewer/Properties/Settings.Designer.cs
+++ TeslaCamViewer/TeslaCamViewer/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace TeslaCamViewer.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
         public bool EnableAutoSearch {
             get {
                 return ((bool)(this["EnableAutoSearch"]));
